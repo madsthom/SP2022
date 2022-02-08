d714ec82ea9ccf1997f64556877031266b4c086e
Mads Smed <mthoma18@student.aau.dk>
2022-02-08 14:45:23 +0100

    Make sure testing works

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ constexpr int arr_size = 10'000;
 vector<int> vec(arr_size);
 int c_arr[arr_size];
 
-int main() {
+int main2() {
     for (int i = 0; i < arr_size; i++) {
         c_arr[i] = i;
     }
