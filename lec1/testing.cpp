1e8cc87366744cbbbe576ddad09a7ea8352ada2f
Mads Smed <mthoma18@student.aau.dk>
2022-02-08 14:45:06 +0100

    Add doctest header

---
+++ lec1/testing.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Mads Smed on 04/02/2022.
+//
+
---
+++ lib/doctest.h
@@ -0,0 +1,8 @@
+//
+// Created by Mads Smed on 04/02/2022.
+//
+
+#ifndef SP2022_DOCTEST_H
+#define SP2022_DOCTEST_H
+
+#endif //SP2022_DOCTEST_H
